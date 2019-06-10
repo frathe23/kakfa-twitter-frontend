767fc55016cf477b275287d825e4198ac6a45b00
Francesco <francesco.larghi@gmail.com>
2019-06-10 22:40:03 +0200

    Adding first features

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      <button type="button" class="btn btn-primary" v-on:click="sendTweet">Post Tweet</button>
+    </div>
     <sse-timeline/>
   </div>
 </template>
 
 <script>
-import SseTimeline from './components/SSETimeline'
+  import SseTimeline from './components/SSETimeline'
 
-export default {
+  export default {
   name: 'app',
   components: {
     SseTimeline
+  },
+  methods: {
+    sendTweet: function () {
+      this.$http.post("http://192.168.178.45:5000/tweets", {
+        "authors": "paolorossi",
+        "content": "Fantastic tool! #wow #lovethis @lucabianchi @marioverdi",
+        "location": "Milano"
+      }).then(response => {
+        this.$toasted.show('sent post request: ' + response.status)
+      }, response => {
+        this.$toasted.error('error post request: ' + response.status)
+      })
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueSSE from 'vue-sse';
+import VueSSE from 'vue-sse'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Toasted from 'vue-toasted';
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(VueSSE);
+Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+Vue.use(Toasted, {
+  duration: 2000
+});
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
