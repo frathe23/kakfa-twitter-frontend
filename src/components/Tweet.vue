e48e9527e4f5a7f422b2e35d470bbd32d6621405
Francesco <francesco.larghi@gmail.com>
2019-06-14 02:15:23 +0200

    Adding Tweet.vue

---
+++ src/components/Tweet.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <b-card
+                :header=tweet.authors
+                header-tag="header"
+                :footer=tweet.timestamp
+                footer-tag="footer"
+        >
+            <b-card-text>{{ tweet.content }}</b-card-text>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tweet',
+    props: {
+        tweet: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
